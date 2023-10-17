351776b12573d7e7aed652fab58ebbfa664996ee
Kelly Anderson <anderske@frib.msu.edu>
2023-10-17 16:03:50 -0400

    Initial commit

---
+++ examples/example.py
@@ -0,0 +1,52 @@
+import math
+import matplotlib.pyplot as plt
+import rust_maps
+
+
+def plot_map(x: list, y: list, title: str, x_label: str, y_label: str, fn: str) -> None:
+    fig, ax = plt.subplots()
+
+    ax.plot(x, y, linestyle=None, marker="o")
+
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+    fig.savefig(f"{fn}.png", dpi=300)
+
+    return
+
+
+def main() -> None:
+    N = 1000  # 1000 iterations
+
+    # Standard Map
+    k = -1
+    ti = 0.1  # 0.1 radians
+    pi = 0  # 0 momentum
+
+    # Pendulum
+    t0 = 45 * math.pi / 180  # 45 degrees
+    p0 = 0  # 0 velocity
+    w = 2 * math.pi * 0.5  # 0.5 Hz
+    dt = 1e-2  # 10 ms
+    total_time = dt * N  # 10 s
+
+    x, y = rust_maps.standard_map_tracking(ti, pi, k, N)
+    plot_map(x, y, "Standard Map", r"$\theta$", "p", "standard-map")
+
+    x, y = rust_maps.pendulum_tracking(t0, p0, w, dt, N)
+    plot_map(
+        x,
+        y,
+        f"Pendulum: {total_time}s (step {dt}s)",
+        r"$\theta$",
+        r"$\dot{\theta}$",
+        "pendulum",
+    )
+
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/lib.rs
@@ -0,0 +1,96 @@
+use pyo3::prelude::*;
+use std::f64::consts::PI;
+
+// * Helper functions
+
+// Pendulum
+fn a_pendulum(x: f64, w: f64) -> f64 {
+    return -w.powf(2.0) * x.sin();
+}
+
+fn yoshida_n4_sub_step(term: f64, cd: f64, ap: f64, dt: f64) -> f64 {
+    return term + cd * ap * dt;
+}
+
+fn yoshida_n4_step_pendulum(x: f64, p: f64, c: f64, d: f64, dt: f64, w: f64) -> (f64, f64) {
+    let nx: f64;
+    let np: f64;
+    let anx: f64;
+
+    nx = yoshida_n4_sub_step(x, c, p, dt);
+    anx = a_pendulum(nx, w);
+    np = yoshida_n4_sub_step(p, d, anx, dt);
+
+    return (nx, np);
+}
+
+fn yoshida_n4_pendulum(x: f64, p: f64, dt: f64, w: f64) -> (f64, f64) {
+    let c1: f64 = 1_f64 / 2_f64 / (2_f64 - 2_f64.powf(1_f64 / 3_f64));
+    let c4: f64 = c1;
+    let c2: f64 = (1_f64 - 2_f64.powf(1_f64 / 3_f64)) * c1;
+    let c3: f64 = c2;
+    let d1: f64 = 1_f64 / (2_f64 - 2_f64.powf(1_f64 / 3_f64));
+    let d3: f64 = d1;
+    let d2: f64 = -(2_f64.powf(1_f64 / 3_f64)) / (2_f64 - 2_f64.powf(1_f64 / 3_f64));
+    let d4: f64 = 0_f64;
+    let carr: [f64; 4] = [c1, c2, c3, c4];
+    let darr: [f64; 4] = [d1, d2, d3, d4];
+    let n: i32 = 4;
+    let mut xn: f64 = x;
+    let mut pn: f64 = p;
+
+    for i in 0..n {
+        (xn, pn) = yoshida_n4_step_pendulum(xn, pn, carr[i as usize], darr[i as usize], dt, w);
+    }
+
+    return (xn, pn);
+}
+
+// Standard map
+fn standard_map(theta: f64, p: f64, k: f64) -> (f64, f64) {
+    let np: f64 = (p + k * theta.sin()) % (2_f64 * PI);
+    let ntheta: f64 = (theta + np) % (2_f64 * PI);
+
+    return (ntheta, np);
+}
+
+// * Python functions
+#[pyfunction]
+fn pendulum_tracking(theta: f64, theat_dot: f64, w: f64, dt: f64, n: i32) -> (Vec<f64>, Vec<f64>) {
+    let mut theta_out: Vec<f64> = vec![0.0; n as usize];
+    let mut theta_dot_out: Vec<f64> = vec![0.0; n as usize];
+    let mut x: f64 = theta;
+    let mut p: f64 = theat_dot;
+
+    for i in 0..n {
+        theta_out[i as usize] = x;
+        theta_dot_out[i as usize] = p;
+        (x, p) = yoshida_n4_pendulum(x, p, dt, w);
+    }
+
+    return (theta_out, theta_dot_out);
+}
+
+#[pyfunction]
+fn standard_map_tracking(theta: f64, p: f64, k: f64, n: i32) -> (Vec<f64>, Vec<f64>) {
+    let mut theta_out: Vec<f64> = vec![0.0; n as usize];
+    let mut p_out: Vec<f64> = vec![0.0; n as usize];
+    let mut x: f64 = theta;
+    let mut y: f64 = p;
+
+    for i in 0..n {
+        theta_out[i as usize] = x;
+        p_out[i as usize] = y;
+        (x, y) = standard_map(x, y, k);
+    }
+
+    return (theta_out, p_out);
+}
+
+/// * A Python module implemented in Rust.
+#[pymodule]
+fn py_rust_maps(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(pendulum_tracking, m)?)?;
+    m.add_function(wrap_pyfunction!(standard_map_tracking, m)?)?;
+    Ok(())
+}
