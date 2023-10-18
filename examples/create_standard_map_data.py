01c014e1140aced441672984bc108ad5dcc07547
Kelly Anderson <anderske@frib.msu.edu>
2023-10-18 18:53:21 -0400

    initial commit

---
+++ examples/create_standard_map_data.py
@@ -0,0 +1,37 @@
+import math
+import numpy as np
+import py_rust_maps
+
+def transpose_list(x: list) -> list:
+    return np.array(x).T.tolist()
+
+def save_3d_data(data: list, filename: str) -> None:
+    data_2d = np.array(data).reshape(np.shape(data)[0], -1)
+    np.savetxt(filename, data_2d, delimiter=",")
+    return
+
+def main() -> None:
+    number_iterations = 200_000
+    number_initial_conditions = 20
+    maximum_amplitude = math.pi / 3
+    filename = "standard_map_data.csv"
+
+    # Standard Map
+    # ti = 0.15
+    k = -1
+    initial_p = 0
+
+    data = []
+    for initial_theta in np.linspace(0, maximum_amplitude, number_initial_conditions):
+        theta, p = py_rust_maps.standard_map_tracking(initial_theta, initial_p, k, number_iterations)
+        data.append(transpose_list([theta, p]))
+        # transpose_list is needed because data should be 
+        # (number_initial_conditions, 2, number_iterations)
+
+    save_3d_data(data, filename)
+    
+    return
+
+
+if __name__ == "__main__":
+    main()
